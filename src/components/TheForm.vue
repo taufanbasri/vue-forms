e9bcaaa6987d6b0a5487edda5260fb321f356f1d
Taufan Basri <taufanprasetyobasri@gmail.com>
2022-05-26 06:25:54 +0700

    Basic Validation

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,8 +1,9 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div class="form-control" :class="{invalid: userNameValidity === 'invalid'}">
       <label for="user-name">Your Name</label>
-      <input v-model="userName" id="user-name" name="user-name" type="text" />
+      <input v-model.trim="userName" @blur="validateInput" id="user-name" name="user-name" type="text" />
+      <p v-if="userNameValidity === 'invalid'">Please enter a valid name!</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
@@ -65,12 +66,20 @@ export default {
       referrer: 'wom',
       interest: [],
       how: null,
-      confirm: false
+      confirm: false,
+      userNameValidity: ''
     }
   },
   methods: {
     submitForm() {
 
+    },
+    validateInput() {
+      if (this.userName === '') {
+        this.userNameValidity = 'invalid'
+      } else {
+        this.userNameValidity = 'valid'
+      }
     }
   },
 }
@@ -88,6 +97,14 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+}
+
+.form-control.invalid input {
+  border-color: red;
+}
+
+.form-control.invalid label {
+  color: red;
 }
 
 label {
